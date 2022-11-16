32fe57b92aa4532007af0085e671a1c49e5b4345
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-11-17 01:31:07 +0700

    navigation

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:flutter/material.dart';
@@ -7,6 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 import '../../shared/shared.dart';
 
 part 'home_page/home_page.dart';
+part 'main_page/main_page.dart';
 part 'splash_page/splash_page.dart';
 part 'sign_in_page/sign_in_page.dart';
 part 'sign_up_page/sign_up_page.dart';
