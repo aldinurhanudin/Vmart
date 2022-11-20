7ca34494600ed2a4fc22a5c3053c31cf4502bd04
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-11-20 20:22:44 +0700

    header

--- lib/ui/pages/home/profile_page/profile_page.dart
+++ lib/ui/pages/home/profile_page/profile_page.dart
@@ -5,11 +5,63 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     return Center(
-      child: Text(
-        'Profile Page',
-       style: primaryTextStyle,
+
+    Widget header(){
+         return AppBar(
+      backgroundColor: backgroundColor1,
+      automaticallyImplyLeading: false,
+      elevation: 0,
+      flexibleSpace: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(
+            defaultMargin,
+          ),
+          child: Row(
+            children: [
+              ClipOval(
+                child: Image.asset(
+                  'assets/image_profile.png',
+                  width: 64,
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hallo, Aldi',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    Text(
+                      '@aldi',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Image.asset(
+                'assets/button_exit.png',
+                width: 20,
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+ 
+    }
+     return Column(
+      children: [
+        header(),
+      ],
     );
   }
 }
