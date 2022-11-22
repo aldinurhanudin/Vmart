598f04ce9d0544655266192924cf3e0a134612f8
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-11-22 10:06:01 +0700

    perbaikan

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'dart:js_util';
+// import 'dart:js_util';
 
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+// import 'dart:js_util';
 import '../../shared/shared.dart';
 
 part 'home_page/product_card/product_card.dart';
