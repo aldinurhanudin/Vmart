7bbe4658e535b497428fc3bcfb55629a20411948
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-11-22 21:53:54 +0700

    gallery

---
+++ lib/models/models.dart
@@ -0,0 +1,4 @@
+import 'category_model/category_model.dart';
+import 'gallery_model/gallery_model.dart';
+
+part 'product_model/product_model.dart';
---
+++ lib/models/product_model/product_model.dart
@@ -0,0 +1,53 @@
+part of '../models.dart';
+
+class ProductModel {
+  int? id;
+  String? name;
+  double? price;
+  String? description;
+  String? tags;
+  CategoryModel? category;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+  late List<GalleryModel> galleries;
+
+  ProductModel({
+    this.id,
+    this.name,
+    this.price,
+    this.description,
+    this.tags,
+    this.category,
+    this.createdAt,
+    this.updatedAt,
+    required this.galleries,
+  });
+
+  ProductModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    price = double.parse(json['price'].toString());
+    description = json['description'];
+    tags = json['tags'];
+    category = CategoryModel.fromJson(json['category']);
+    galleries = json['galleries']
+        .map((gallery) => GalleryModel.fromJson(gallery))
+        .toList();
+    createdAt = DateTime.parse(json['created_at']);
+    updatedAt = DateTime.parse(json['updated_at']);
+  }
+
+  Map<String, dynamic> toJson(){
+    return{
+      'id': id,
+      'name': name,
+      'price': price,
+      'description': description,
+      'tags': tags,
+      'category': category!.toJson(),
+      'galleries': galleries.map((gallery) => gallery.toJson()).toList(),
+      'createdAt': createdAt.toString(),
+      'updatedAt': updatedAt.toString(),
+    };
+  }
+}
