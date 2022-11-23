58e14c905703a10da8df75dad32cf1b66bcc2dc0
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2022-11-23 21:57:30 +0700

    provider

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -5,3 +5,4 @@ import '../models/user_model/user_model.dart';
 
 part 'auth_service/auth_service.dart';
 part 'product_service/product_service.dart';
+part 'transaction_service/transaction_service.dart';
