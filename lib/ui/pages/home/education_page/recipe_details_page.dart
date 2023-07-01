2b2fcfb92a2ace5f1351d9e2cfc98e26fc1f93ba
Aldi Nurhanudin <aldinurhanudin08@gmail.com>
2023-07-02 00:37:04 +0700

    perbaikan tampilan detailresep

--- lib/ui/pages/home/education_page/recipe_details_page.dart
+++ lib/ui/pages/home/education_page/recipe_details_page.dart
@@ -2,7 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:provider/provider.dart';
+import 'package:vmart/providers/providers.dart';
 import 'package:vmart/shared/shared.dart';
+import 'package:vmart/ui/widgets/widgets.dart';
+
+import '../../pages.dart';
 
 class RecipeDetailsPage extends StatefulWidget {
   final DocumentSnapshot data;
@@ -29,6 +34,7 @@ class _RecipeDetailsPageState extends State<RecipeDetailsPage> {
 
   @override
   Widget build(BuildContext context) {
+    ProductProvider productProvider = Provider.of<ProductProvider>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -39,6 +45,14 @@ class _RecipeDetailsPageState extends State<RecipeDetailsPage> {
         title: Text(
           truncateText(widget.data['title'], 5),
           style: TextStyle(color: Colors.black),
+        ),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1.0),
+          child: Divider(
+            color: Colors.grey,
+            height: 1.0,
+            thickness: 1.2,
+          ),
         ),
         actions: [
           ClipRRect(
@@ -92,33 +106,375 @@ class _RecipeDetailsPageState extends State<RecipeDetailsPage> {
                 ),
               ),
               SizedBox(height: 10),
-              Text(
-                '${widget.data['subtitle']}',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.black,
-                ),
-                textAlign: TextAlign.justify,
-                textScaleFactor: 1.0,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'Kategori',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 8),
               Container(
+                padding: EdgeInsets.all(10),
                 decoration: BoxDecoration(
-                  color: Colors.grey,
+                  border: Border.all(
+                    color: Colors.green,
+                    width: 2.0,
+                  ),
                   borderRadius: BorderRadius.circular(10),
                 ),
-                padding: EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    SizedBox(width: 3),
+                    Icon(
+                      Icons.timer_outlined,
+                      color: Colors.green,
+                      size: 40,
+                    ),
+                    SizedBox(width: 5),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Waktu Pembuatan',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          '20 menit',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 13),
+                    Icon(
+                      Icons.people,
+                      color: Colors.green,
+                      size: 45,
+                    ),
+                    SizedBox(width: 5),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Porsi:',
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          '2-3 orang',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: primaryColor,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                ),
                 child: Text(
-                  '${widget.data['kategori']}',
-                  style: TextStyle(fontSize: 18),
+                  'Beli Bahannya di Sini',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 15),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        Row(
+                          children: productProvider.products
+                              .take(6)
+                              .map(
+                                (product) => ClipRect(
+                                  child: Align(
+                                    alignment: Alignment.topLeft,
+                                    widthFactor: 1.0,
+                                    child: ProductCard(product),
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => AllProductsPage(),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            width: 215,
+                            height: 278,
+                            margin: EdgeInsets.only(
+                              right: defaultMargin,
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.green,
+                                width: 1.0,
+                              ),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                            ),
+                            child: Center(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.arrow_forward_ios_outlined,
+                                    color: Colors.green,
+                                  ),
+                                  Text(
+                                    'Lihat Semua',
+                                    style: TextStyle(
+                                      color: Colors.green,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: primaryColor,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                ),
+                child: Text(
+                  'Bahan yang Dibutuhkan',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, top: 10),
+                child: Text(
+                  '${widget.data['cara1']}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.justify,
+                  textScaleFactor: 1.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, top: 10),
+                child: Text(
+                  '${widget.data['bahan2']}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.justify,
+                  textScaleFactor: 1.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, top: 10),
+                child: Text(
+                  '${widget.data['bahan3']}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.justify,
+                  textScaleFactor: 1.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, top: 10),
+                child: Text(
+                  '${widget.data['bahan4']}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.justify,
+                  textScaleFactor: 1.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, top: 10),
+                child: Text(
+                  '${widget.data['bahan5']}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.justify,
+                  textScaleFactor: 1.0,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: 200,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: primaryColor,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                ),
+                child: Text(
+                  'Cara Membuat',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, top: 10),
+                child: Text(
+                  '${widget.data['cara1']}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.justify,
+                  textScaleFactor: 1.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, top: 10),
+                child: Text(
+                  '${widget.data['cara2']}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.justify,
+                  textScaleFactor: 1.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, top: 10),
+                child: Text(
+                  '${widget.data['cara3']}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.justify,
+                  textScaleFactor: 1.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, top: 10),
+                child: Text(
+                  '${widget.data['cara4']}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.justify,
+                  textScaleFactor: 1.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10, top: 10),
+                child: Text(
+                  '${widget.data['cara5']}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.justify,
+                  textScaleFactor: 1.0,
+                ),
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Container(
+                width: 390,
+                height: 45.0,
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(20.0),
+                  border: Border.all(
+                    color: Colors.green,
+                    width: 2.0,
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.only(top: 2, left: 5),
+                      child: Icon(
+                        Icons.search,
+                        color: primaryColor,
+                        size: 35,
+                      ),
+                    ),
+                    Container(
+                      width: 292,
+                      // height: 63.0,
+                      decoration: BoxDecoration(
+                        color: primaryColor,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          'Lihat Resep Menarik Lainnya di Sini',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
